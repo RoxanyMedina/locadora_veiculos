9040af54aea613734c75d4c3dc033226f0079f37
RoxanyMedina <roxanyhelena@gmail.com>
2026-07-27 16:49:21 -0300

    Add files via upload

---
+++ locadora_veiculos/moto.py
@@ -0,0 +1,22 @@
+from veiculo import Veiculo
+
+class moto(Veiculo):
+    def __init__(self, modelo, placa, ano, disponivel, valor_diaria):
+        super().__init__(modelo, placa, ano, disponivel, valor_diaria)
+        
+    def alugar(self, dias):
+        if self.disponivel:
+            valor = self.calcular_diaria(dias)
+            
+            self.disponivel = False
+            
+        # print("Não tá disponível")    
+            
+    def devolver(self, dias):
+        valor = self.calcular_diaria(dias)
+        print(f"Valor total {valor}")
+        
+    def calcular_diaria(self, dias):
+        if dias > 7:
+            print
+            return (self.valor_diaria * 0.9) * dias
